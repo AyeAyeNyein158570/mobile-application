8cd59c79b10745c0d18025623e0f84a86eaa4e65
AyeAyeNyein158570 <s257118@gs.sundaidenshi.ac.jp>
2026-06-19 13:28:21 +0900

    Add files via upload

---
+++ 01_omikuji01_09/js/omikuji.js
@@ -0,0 +1,97 @@
+"use strict";
+
+window.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    $("header").textillate({
+      loop: false,
+      minDisplayTime: 9000,
+      initialDelay: 1000,
+      autoStart: true,
+
+      in: {
+        effect: "fadeInLeftBig",
+        delayScale: 1.5,
+        delay: 50,
+        sync: false,
+        shuffle: true,
+      },
+    });
+    $(function () {
+      ScrollReveal().reveal("#btn1", { duration: 9000 });
+    });
+    this.setTimeout(function () {
+      let popMessage = "いらっしゃい！　おみくじ引いてって！";
+      this.window.alert(popMessage);
+    }, "3000");
+  },
+  false
+);
+
+const btn1 = document.getElementById("btn1");
+const omikujiText = document.getElementById("omikujiText");
+btn1.addEventListener(
+  "click",
+  function () {
+    let resultText = [
+      "大吉!!!!!",
+      "吉!!!!",
+      "中吉!!!",
+      "小吉",
+      "末吉!",
+      "凶。。",
+    ];
+    let resultColor = [
+      "#ff0000",
+      "#c71585",
+      "#ff1493",
+      "#ff69b4",
+      "#ff8c00",
+      "#1e90ff",
+    ];
+    let resultFontSize = ["80px", "70px", "60px", "50px", "40px", "30px"];
+    let resultMaxSpeed = [20, 15, 10, 5, 5, 5];
+    let resultMaxSize = [35, 30, 20, 10, 20, 30];
+    let resultMinSize = [20, 15, 10, 8, 10, 15];
+    let resultImage = [
+      "img/star.png",
+      "img/sakura_hanabira.png",
+      "img/sakura_hanabira.png",
+      "img/sakura_hanabira.png",
+      "img/leaf.png",
+      "img/snowflakes.png",
+    ];
+    let n = Math.floor(Math.random() * resultText.length);
+    omikujiText.textContent = resultText[n];
+    omikujiText.style.color = resultColor[n];
+    omikujiText.style.fontSize = resultFontSize[n];
+    // switch (n) {
+    //   case 0:
+    //     omikujiText.textContent = "Very Happy!!!";
+    //     omikujiText.style.color = "blue";
+    //     omikujiText.style.fontSize = "45px";
+    //     break;
+    //   case 1:
+    //     omikujiText.textContent = "Happy!!!";
+    //     omikujiText.style.color = "red";
+    //     omikujiText.style.fontSize = "40px";
+    //     break;
+    //   case 2:
+    //     omikujiText.textContent = "UnHappy";
+    //     omikujiText.style.color = "black";
+    //     omikujiText.style.fontSize = "20px";
+    //     break;
+    // }
+    $(document).snowfall("clear");
+    $(document).ready(function () {
+      $(document).snowfall({
+        maxSpeed: resultMaxSpeed[n],
+        minSpeed: 1,
+        maxSize: resultMaxSize[n],
+        minSize: resultMinSize[n],
+        image: resultImage[n],
+      });
+    });
+  },
+  false
+);
